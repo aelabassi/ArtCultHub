b6247b90cb22137ab95b2d54cc9c3bf44dcb338d
Lelaabk <lolabkari@gmail.com>
2025-01-03 04:31:37 +0100

    Header section

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Routers from "../../routes/Routers";
+import Header from "../Header/Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Routers />
+      </div>
+
+    </div>
+  );
+};
+
+export default Layout;
