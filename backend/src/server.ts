c1b4d2d8498ec13f18686054a9beb60522e3762f
nancie6695 <nanciek004@gmail.com>
2025-01-03 09:13:35 +0300

    Connecting to postgres and updates on server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
-import express, {Response, Request} from 'express';
-import config from './config';
+import express, { Response, Request } from 'express';
 import morgan from 'morgan';
+import config from './config';
+import artRoutes from './routes/artRoutes';
+
 const app = express();
 
 // middlewares
@@ -9,13 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // handlers
-
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello to art Backend');
 });
+
+// Routes
+app.use('/api', artRoutes); 
 
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
 });
 
+export default app;
