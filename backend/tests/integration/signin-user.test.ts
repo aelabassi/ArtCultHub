3cbd0bbb43f8f450b4bbdf27f7fea2efba1c98da
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2025-01-20 12:38:55 +0100

    more tests

--- backend/tests/integration/signin-user.test.ts
+++ backend/tests/integration/signin-user.test.ts
@@ -1,5 +1,5 @@
-import {test, vi, expect, describe } from 'vitest'
-import { Response, Request } from 'express';
+import {test, vi, expect, describe }  from 'vitest'
+import { Response, Request }  from 'express';
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
 import UserRouter from '../../src/routes/user';
