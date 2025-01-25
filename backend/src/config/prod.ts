254892f0fdc2389999544fb04e00369c8dd7f970
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2025-01-25 16:36:29 +0100

    added production code

---
+++ backend/src/config/prod.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+  port: 5000,
+  secret: {
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpire: process.env.JWT_EXP,
+    dbUrl: process.env.MONGO_URL_PP,
+  },
+}
